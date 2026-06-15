f408f2107fc8db06782b3cf75cfc8207cc04de9c
Puff <owenpowerr3@gmail.com>
2026-06-15 20:55:52 +0100

    Update branch contents

--- js/inline_2.1781552879424.js
+++
@@ -1,4 +0,0 @@
-
-function toggleMenu() {
-  document.getElementById("mobileMenu").classList.toggle("show");
-}
---
+++ js/inline_2.1781553345351.js
@@ -0,0 +1,8 @@
+
+document.getElementById("hamburger")
+  .addEventListener("click", function () {
+    document.getElementById("mobileMenu").classList.toggle("show");
+  });
+function toggleMenu() {
+  document.getElementById("mobileMenu").classList.toggle("show");
+}
