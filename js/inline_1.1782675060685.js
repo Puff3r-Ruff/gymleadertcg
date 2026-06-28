3046011ef732d55ee4e522d1f6560a0589e4fb59
Puff <owenpowerr3@gmail.com>
2026-06-28 20:31:14 +0100

    Update branch contents

--- js/inline_1.1782675060685.js
+++ js/inline_1.1782675060685.js
@@ -1,37 +1,33 @@
 
 // Load prefab list from /prefabs/prefabs.json
+// Load prefab list from /prefabs/prefabs.json
 fetch("/prefabs/prefabs.json")
   .then(res => res.json())
   .then(prefabs => {
-    const select = document.getElementById("prefabSelect");
-    select.innerHTML = ""; // clear loading text
 
-    prefabs.forEach(file => {
-      const opt = document.createElement("option");
-      opt.value = file;
-      opt.textContent = file.replace(".html", "");
-      select.appendChild(opt);
-    });
+    // Read ?Template=folder/file.html from URL
+    const params = new URLSearchParams(window.location.search);
+    const template = params.get("Template");
 
-    // Auto-load first prefab
-    if (prefabs.length > 0) {
-      window.currentTheme = prefabs[0].split("/")[0];   // ⭐ FIX
-      loadPrefab(prefabs[0]);
+    if (template) {
+      // Validate that the template exists in the prefab list
+      if (prefabs.includes(template)) {
+        window.currentTheme = template.split("/")[0];
+        loadPrefab(template);
+      } else {
+        console.error("Template not found in prefab list:", template);
+      }
+    } else {
+      // Fallback: load first prefab
+      if (prefabs.length > 0) {
+        window.currentTheme = prefabs[0].split("/")[0];
+        loadPrefab(prefabs[0]);
+      }
     }
-
-    // When user selects a prefab
-    select.onchange = () => {
-      const selected = select.value;
-      window.currentTheme = selected.split("/")[0];     // ⭐ FIX
-      loadPrefab(selected);
-    };
   })
   .catch(err => {
     console.error("Could not load prefab list:", err);
-    document.getElementById("prefabSelect").innerHTML =
-      "<option>Error loading prefabs</option>";
   });
-
 
 function rewritePaths(root, prefabDir) {
   // Fix all src attributes
